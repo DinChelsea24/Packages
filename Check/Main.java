b2dab818c36be39f17769c40ea8a88c91ecb60f5
Din <minh.provmusic@gmail.com>
2024-07-12 19:33:03 +0700

    Version 1

---
+++ Check/Main.java
@@ -0,0 +1,11 @@
+package Check;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Student s = new Student();
+        Scanner sc = new Scanner(System.in);
+    }
+}
